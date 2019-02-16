4eac393c4d79d818277889dc7d047c921bdb33f4
Systemy mobilne <systemymobilne@mac-mini-2.local>
2019-02-16 09:13:22 +0100

    Initial Commit

---
+++ TrCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrCalculator
+//
+//  Created by Systemy mobilne on 16.02.2019.
+//  Copyright © 2019 Systemy mobilne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
